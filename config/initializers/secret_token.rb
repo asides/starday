6fb3a603aa84a12a0116d6cbd336d717675195ba
asides <yaroslav.str@gmail.com>
2013-10-25 15:24:04 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Starday::Application.config.secret_key_base = '2b7e4968fc7469c0e03b074214d2afffcb236a487d957fc8f2b3b6d45bf8bada2f3a36512475c14e37fbb24c2702b1251578ea87a7854696b842bd10f8279b50'
