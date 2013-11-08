a948d1fcc2cc9f1bd0300d0de5a7ccb9e84fbfb3
asides <yaroslav.str@gmail.com>
2013-11-08 18:16:24 +0200

    add money decorator for display "грн"

---
+++ config/initializers/money.rb
@@ -0,0 +1,14 @@
+Money::Currency.register({
+                             :priority=> 100,
+                             :iso_code=> "UAH",
+                             :name=> "Ukrainian Hryvnia",
+                             :symbol=> "грн",
+                             :alternate_symbols=> [],
+                             :subunit=> "Kopiyka",
+                             :subunit_to_unit=> 100,
+                             :symbol_first=> false,
+                             :html_entity=> "грн",
+                             :decimal_mark=> ".",
+                             :thousands_separator=> ",",
+                             :iso_numeric=> "980"
+                         })
